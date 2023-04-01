8b1bf04198230e07b30926cd174c48de35908733
nicki1002 <nicole18hernandez@hotmail.com>
2023-03-31 22:37:16 -0600

    Finalizacion clase 11

--- src/main/java/com/TiendaM_IQ2023/controller/ClienteController.java
+++ src/main/java/com/TiendaM_IQ2023/controller/ClienteController.java
@@ -1,4 +1,3 @@
-
 package com.TiendaM_IQ2023.controller;
 
 import com.TiendaM_IQ2023.domain.Cliente;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class ClienteController {
 
@@ -20,10 +18,16 @@ public class ClienteController {
 
     @GetMapping("/cliente/listado")
     public String inicio(Model model) {
-
         var clientes = clienteService.getClientes();
-        model.addAttribute("clientes", clientes);
 
+        var limiteTotal = 0;
+        for (var c : clientes) {
+            limiteTotal += c.getCredito().getLimite();
+        }
+        model.addAttribute("limiteTotal", limiteTotal);
+        model.addAttribute("totalClientes", clientes.size());
+
+        model.addAttribute("clientes", clientes);
         return "/cliente/listado";
     }
 
@@ -51,10 +55,11 @@ public class ClienteController {
         return "redirect:/cliente/listado";
     }
 
-   @GetMapping("/cliente/encontrar")
+    @GetMapping("/cliente/encontrar")
     public String encontrarCliente(Cliente cliente) {
         return "/cliente/buscar";
     }
+
     @GetMapping("/cliente/buscar")
     public String buscarCliente(Cliente cliente, String apellidos, Model model) {
         List<Cliente> clientes = clienteService.findByApellidos(apellidos);
